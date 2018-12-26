4c99c6ae7adfbee7c1f09ac59af903ed10b485a6
Nick Torbich <nicktorbich@protonmail.com>
2018-12-26 02:07:55 +0100

    Update mongoose url parser options

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/Todo');
+mongoose.connect('mongodb://localhost/Todo', { useNewUrlParser: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
