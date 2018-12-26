d20d43652b53c8f4d76e937c15650172ea0afc11
Nick Torbich <nicktorbich@protonmail.com>
2018-12-26 02:07:36 +0100

    Add test for GET /todos route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -3,9 +3,20 @@ const request = require('supertest');
 
 const { app } = require('../server');
 const { Todo } = require('../models/todo');
+const todos = [
+  { text: 'First todo' },
+  { text: 'Second todo' },
+]
 
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.deleteMany({}).then(() => {
+    Todo.insertMany(todos, (err, todos) => {
+      if (err) {
+        return done(err);
+      }
+      done();
+    });
+  });
 });
 
 describe('POST /todos', () => {
@@ -48,10 +59,22 @@ describe('POST /todos', () => {
 
         Todo.find()
           .then((todos) => {
-            expect(todos.length).toBe(0);
+            expect(todos.length).toBe(2);
             done();
           })
           .catch(err => done(err));
       });
+  });
+});
+
+describe('GET /todos', () => {
+  it('should list all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2)
+      })
+      .end(done);
   });
 });
