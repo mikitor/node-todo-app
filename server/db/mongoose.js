9ac9482241a27748003100e750356f0e910a4470
Nick Torbich <nicktorbich@protonmail.com>
2018-12-28 00:58:51 +0100

    Setup for Heroku deployment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/Todo', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Todo', { useNewUrlParser: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // create application/json parser
 const jsonParser = bodyParser.json();
