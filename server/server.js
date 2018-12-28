eb081afd54909df50ff5cf73dd3541213437bcde
Nick Torbich <nicktorbich@protonmail.com>
2018-12-28 18:39:26 +0100

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 const { ObjectID } = require('mongodb');
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -64,6 +65,38 @@ app.delete('/todos/:id', (req, res) => {
   }).catch((err) => {
     res.status(400).send();
   });
+});
+
+app.patch('/todos/:id', jsonParser, (req, res) => {
+  const { id } = req.params;
+  const body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo
+    .findOneAndUpdate(
+      { _id: id },
+      { $set: body },
+      { new: true },
+    )
+    .then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo });
+    })
+    .catch((err) => {
+      res.status(400).send();
+    });
 });
 
 app.listen(port, () => console.log(`Started on port ${port}`));
