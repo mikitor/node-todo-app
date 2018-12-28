942684c95950f94dc1c64717bf4c646038eb4890
Nick Torbich <nicktorbich@protonmail.com>
2018-12-28 01:48:41 +0100

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -51,6 +51,21 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
+app.delete('/todos/:id', (req, res) => {
+  const { id } = req.params;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findByIdAndDelete(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({ todo });
+  }).catch((err) => {
+    res.status(400).send();
+  });
+});
+
 app.listen(port, () => console.log(`Started on port ${port}`));
 
 module.exports = {
